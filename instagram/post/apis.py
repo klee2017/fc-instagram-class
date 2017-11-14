a535f492652ad4139d9746b748e73355f6c65219
Kyeong Lee <kaythechemist@gmail.com>
2017-11-14 15:59:44 +0900

    permissions util모듈 추가, PostLikeToggle구현

--- instagram/post/apis.py
+++ instagram/post/apis.py
@@ -2,8 +2,11 @@
 # 근데 APIView를 상속받도록
 # Serializer도 생성 (serializers.py)
 # get요청만 응답
-from rest_framework import mixins, generics
+from rest_framework import generics, permissions
+from rest_framework.response import Response
 
+from member.serializers import UserSerializer
+from utils.permissions import IsAuthorOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
@@ -11,6 +14,36 @@ from .serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (
+        IsAuthorOrReadOnly,
+    )
+
+
+class PostLikeToggle(generics.GenericAPIView):
+    queryset = Post.objects.all()
+    lookup_url_kwarg = 'post_pk'
+
+    def post(self, request, *args, **kwargs):
+        instance = self.get_object()
+        user = request.user
+        if instance in user.like_posts.all(pk=instance.pk):
+            user.like_posts.remove(instance)
+            like_status = False
+        else:
+            user.like_posts.add(instance)
+            like_status = True
+        data = {
+            'user': UserSerializer(user).data,
+            'post': PostSerializer(instance).data,
+            'result': like_status,
+        }
+        return Response(data)
--- instagram/post/urls/apis.py
+++ instagram/post/urls/apis.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from .. import apis
 
 urlpatterns = [
-    url(r'^$', apis.PostList.as_view(), name='api-post'),
+    url(r'^$', apis.PostList.as_view(), name='post-list'),
+    url(r'^(?P<pk>\d+)/$', apis.PostDetail.as_view(), name='post-detail'),
+    url(r'^(?P<post_pk>\d+)/like-toggle/$', apis.PostLikeToggle.as_view(), name='post-detail'),
 ]
